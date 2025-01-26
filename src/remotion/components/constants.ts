9126f4962b20d3254ea886b1a4c15935f3403c9e
Gérald Leban <gerald.leban0@gmail.com>
2025-01-26 19:10:31 +0100

    chore: change structure

--- src/remotion/components/constants.ts
+++
@@ -1,6 +0,0 @@
-export const COMPOSITION_WIDTH = 400;
-export const COMPOSITION_HEIGHT = 400;
-
-export const FPS = 30;
-
-export const DURATION = FPS * 38;
---
+++ src/remotion/constants.ts
@@ -0,0 +1,41 @@
+export const config = {
+	compositionHeight: 600,
+	compositionWidth: 600,
+	duration: 1000,
+	fps: 30
+};
+
+export type ThemeId = 'player1' | 'player2';
+
+export type Theme = {
+	name: ThemeId;
+	displayName: string;
+	colors: {
+		background: string;
+		black: string;
+		white: string;
+		primary: string;
+	};
+};
+
+export const player1Theme: Theme = {
+	name: 'player1',
+	displayName: 'Player 1',
+	colors: {
+		background: '#1a1a1a',
+		black: '#141414',
+		white: '#ffffff',
+		primary: '#FD3132'
+	}
+};
+
+export const player2Theme: Theme = {
+	name: 'player2',
+	displayName: 'Player 2',
+	colors: {
+		background: '#1a1a1a',
+		black: '#141414',
+		white: '#ffffff',
+		primary: '#3189FD'
+	}
+};
