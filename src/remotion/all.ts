d4849d38a625bc324013b1bb598fa5ea028bf80c
Gérald Leban <gerald.leban0@gmail.com>
2025-01-29 22:06:46 +0100

    refactor(data): add a top 3 of the most attended tournaments

--- src/remotion/all.ts
+++ src/remotion/all.ts
@@ -58,16 +58,44 @@ export const fakeData = {
 			{ month: 11, total: 11 },
 			{ month: 12, total: 6 }
 		],
-		mostAttendees: {
-			id: 659671,
-			name: 'Sunrise 2024',
-			startAt: 1729929600,
-			hasOfflineEvents: true,
-			hasOnlineEvents: false,
-			numAttendees: 212,
-			city: 'Hyères',
-			countryCode: 'FR'
-		},
+		mostAttendees: [
+			{
+				id: 659671,
+				name: 'Sunrise 2024',
+				startAt: 1729929600,
+				hasOfflineEvents: true,
+				hasOnlineEvents: false,
+				numAttendees: 212,
+				city: 'Hyères',
+				countryCode: 'FR',
+				image:
+					'https://www.bing.com/th?pid=Sgg&qlt=100&u=https%3A%2F%2Fimages.start.gg%2Fimages%2Ftournament%2F659671%2Fimage-69d035b4cd14dfb83decda1ebbd2752a-optimized.png&ehk=8mSAFkGDRo2%2BzCxSdk%2BH6fRomg1vaLNDyUsU6Pdd0Dw%3D&w=280&h=280&r=0'
+			},
+			{
+				id: 659672,
+				name: "TLS MAD'Ness #42 - PK END !",
+				startAt: 1761465600,
+				hasOfflineEvents: true,
+				hasOnlineEvents: false,
+				numAttendees: 107,
+				city: 'Toulouse',
+				countryCode: 'FR',
+				image:
+					'https://www.bing.com/th?pid=Sgg&qlt=100&u=https%3A%2F%2Fimages.start.gg%2Fimages%2Ftournament%2F738440%2Fimage-9ebffce5d0e4b0c5c8dd6c809a74d7ad-optimized.png&ehk=AYqVjOjzZVaBthGQCSXDbzciDyCpgI%2FOxAWfL79jETg%3D&w=280&h=280&r=0'
+			},
+			{
+				id: 659673,
+				name: 'Bergougnoultimate #2',
+				startAt: 1793001600,
+				hasOfflineEvents: true,
+				hasOnlineEvents: false,
+				numAttendees: 14,
+				city: 'Toulouse',
+				countryCode: 'FR',
+				image:
+					'https://www.bing.com/th?pid=Sgg&qlt=100&u=https%3A%2F%2Fimages.start.gg%2Fimages%2Ftournament%2F718810%2Fimage-49f0a13a8773a70cec9ddb1c9e06511e-optimized.png&ehk=T%2FPt%2Btyg%2BPViyVUilM5Sc7EEvGoZsNLIux68h%2FB9jpE%3D&w=280&h=280&r=0'
+			}
+		],
 		total: 108,
 		online: 8,
 		offline: 100
