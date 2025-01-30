15ba979c37530404fcd8ec9a0eb2665e061c820f
Gérald Leban <gerald.leban0@gmail.com>
2025-01-30 09:14:27 +0100

    feat(remotion): add customizable font size and line height

--- src/remotion/_components/Title.tsx
+++ src/remotion/_components/Title.tsx
@@ -4,9 +4,11 @@ import type { Theme } from '../constants';
 export const Title: React.FC<{
 	children: React.ReactNode;
 	className?: string;
+	fontSize?: number;
+	lineHeight?: number;
 	style?: React.CSSProperties;
 	theme: Theme;
-}> = ({ children, className, style, theme }) => {
+}> = ({ children, className, fontSize, lineHeight, style, theme }) => {
 	return (
 		<div
 			className={className}
@@ -15,7 +17,7 @@ export const Title: React.FC<{
 				display: 'flex',
 				justifyContent: 'center',
 				alignItems: 'center',
-				height: 78,
+				minHeight: 78,
 				backgroundColor: theme.colors.primary,
 				border: `8px solid ${theme.colors.black}`,
 				paddingLeft: 32,
@@ -27,11 +29,14 @@ export const Title: React.FC<{
 			<h1
 				className="fot-rodin"
 				style={{
+					fontSize: fontSize ?? 32,
+					lineHeight: lineHeight ?? 1.4,
+					letterSpacing: '-0.04em',
+					textAlign: 'center',
 					color: theme.colors.white,
 					// @ts-expect-error -webkit-text-stroke is not in the types
 					'-webkit-text-stroke': `8px ${theme.colors.black}`,
-					textShadow: `6px 6px ${theme.colors.black}`,
-					letterSpacing: '-0.04em'
+					textShadow: `6px 6px ${theme.colors.black}`
 				}}
 			>
 				{children}
