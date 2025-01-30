61ccce22c636d72b201dcbd37c53e846dac1f36e
Gerald Leban <gerald.leban0@gmail.com>
2025-01-30 09:46:06 +0100

    refactor(remotion): use Series

--- src/remotion/main.tsx
+++ src/remotion/main.tsx
@@ -1,42 +1,55 @@
 import React from 'react';
-import { AbsoluteFill, Sequence } from 'remotion';
+import { AbsoluteFill, Series } from 'remotion';
 import type { CompositionProps } from './types';
 import { ThisIsMyRecap } from './ThisIsMyRecap';
 import { Background } from './_components/Background';
 import { Slide, transitionDuration } from './_components/Slide';
 import { BiggestTournament } from './BiggestTournament';
 import { DURATIONS } from './constants';
+import { TournamentsHeatmap } from './TournamentsHeatmap';
 
 export const Main: React.FC<CompositionProps> = ({ stats, theme }) => {
-	const accumulatedDurations = (i: number) => DURATIONS.slice(0, i).reduce((a, b) => a + b, 0);
-
 	return (
 		<AbsoluteFill>
 			<Background theme={theme} />
 
-			<Sequence durationInFrames={DURATIONS[0] + transitionDuration} name="This is my recap">
-				<Slide direction="left">
-					<ThisIsMyRecap
-						theme={theme}
-						name={stats.player.name}
-						country={'USA'}
-						genderPronouns={stats.player.genderPronouns}
-						image={stats.player.image}
-					/>
-				</Slide>
-			</Sequence>
-
-			<Sequence
-				durationInFrames={DURATIONS[1] + transitionDuration}
-				from={accumulatedDurations(1)}
-				name="Biggest tournament"
-			>
-				<Slide direction="right">
+			<Series>
+				<Series.Sequence durationInFrames={DURATIONS[0]} name="This is my recap">
 					<Slide direction="left">
-						<BiggestTournament stats={stats} theme={theme} />
+						<ThisIsMyRecap
+							theme={theme}
+							name={stats.player.name}
+							country={'USA'}
+							genderPronouns={stats.player.genderPronouns}
+							image={stats.player.image}
+						/>
 					</Slide>
-				</Slide>
-			</Sequence>
+				</Series.Sequence>
+
+				<Series.Sequence
+					durationInFrames={DURATIONS[1]}
+					offset={-transitionDuration}
+					name="Biggest tournament"
+				>
+					<Slide direction="right">
+						<Slide direction="left">
+							<BiggestTournament stats={stats} theme={theme} />
+						</Slide>
+					</Slide>
+				</Series.Sequence>
+
+				<Series.Sequence
+					durationInFrames={DURATIONS[2]}
+					offset={-transitionDuration}
+					name="Tournament Heatmap"
+				>
+					<Slide direction="right">
+						<Slide direction="left">
+							<TournamentsHeatmap theme={theme} />
+						</Slide>
+					</Slide>
+				</Series.Sequence>
+			</Series>
 		</AbsoluteFill>
 	);
 };
