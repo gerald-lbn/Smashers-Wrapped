b06106733294266ec84dd4b6ee58fab548b35cc6
Gérald Leban <gerald.leban0@gmail.com>
2025-01-30 09:14:48 +0100

    refactor(remotion): split Chip component

--- src/remotion/_components/InGameProfile.tsx
+++ src/remotion/_components/InGameProfile.tsx
@@ -1,34 +1,6 @@
-import type { Theme } from '$remotion/constants';
+import type { Theme } from '../constants';
+import { Chip } from './Chip';
 import React from 'react';
-
-const Chip: React.FC<{
-	label: string;
-	theme: Theme;
-}> = ({ label, theme }) => {
-	return (
-		<div
-			style={{
-				border: `4px solid ${theme.colors.black}`,
-				backgroundColor: theme.colors.primary,
-				padding: '6.8px 13.6px'
-			}}
-		>
-			<span
-				className="fot-rodin"
-				style={{
-					fontSize: '13.6px',
-					lineHeight: '20px',
-					color: theme.colors.white,
-					// @ts-expect-error -webkit-text-stroke is not in the types
-					'-webkit-text-stroke': `6px ${theme.colors.black}`,
-					textShadow: `4px 4px ${theme.colors.black}`
-				}}
-			>
-				{label}
-			</span>
-		</div>
-	);
-};
 
 export const InGameProfile: React.FC<{
 	image?: string;
