eb94a0432e1e5cb567c842275ed5db73f90e757d
Gérald Leban <gerald.leban0@gmail.com>
2025-05-09 19:23:43 +0200

    refactor: get rid of urql

--- src/lib/start.gg/queries.ts
+++ src/lib/start.gg/queries.ts
@@ -1,156 +1,22 @@
 import { graphql } from '$lib/graphql';
 
-export const WrappedTournamentsAndSetsOnStream = graphql(`
-	query WrappedTournamentsAndSetsOnStream($playerId: ID!, $videoGameId: ID!) {
-		player(id: $playerId) {
-			id
-			gamerTag
-			prefix
-
-			sets(filters: { hasVod: true, hideEmpty: true }) {
-				pageInfo {
-					total
-				}
-			}
-
-			user {
-				id
-				images(type: "profile") {
-					id
-
-					url
-					ratio
-				}
-
-				tournaments(
-					query: {
-						perPage: 500
-						filter: { past: true, upcoming: false, videogameId: [$videoGameId] }
+export const SearchPlayerByGamerTagQuery = `
+	query SearchByGamerTag($search: PlayerQuery!) {
+		players(query: $search) {
+			nodes {
+				gamerTag
+				prefix
+				user {
+					slug
+					location {
+						country
 					}
-				) {
-					pageInfo {
-						total
-						totalPages
-					}
-					nodes {
-						id
-						name
-						images {
-							id
-							type
-							url
-						}
-						startAt
-						hasOfflineEvents
-						hasOnlineEvents
-						numAttendees
+					images(type: "profile") {
+						url
 					}
 				}
 			}
 		}
 	}
-`);
-
-export const WrappedSets = graphql(`
-	query WrappedSets(
-		$playerId: ID!
-		$tournamentsIds: [ID!]
-		$page: Int
-		$perPage: Int
-		$info: Boolean!
-	) {
-		player(id: $playerId) {
-			id
-
-			sets(
-				filters: {
-					showByes: false
-					tournamentIds: $tournamentsIds
-					# Filtering by entrantSize to avoid showing sets with more than 2 entrants is not working
-					# https://discord.com/channels/339548254704369677/504697661795074058/930881754326265907
-					# entrantSize: [2]
-				}
-				page: $page
-				perPage: $perPage
-			) {
-				pageInfo @include(if: $info) {
-					page
-					total
-					totalPages
-				}
-
-				nodes @skip(if: $info) {
-					id
-
-					displayScore
-					winnerId
-
-					game(orderNum: 1) {
-						id
-
-						selections {
-							id
-
-							entrant {
-								id
-								name
-								initialSeedNum
-								participants {
-									id
-
-									player {
-										id
-
-										gamerTag
-										prefix
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-`);
-
-export const WrappedSelections = graphql(`
-	query WrappedSelections(
-		$playerId: ID!
-		$tournamentsIds: [ID!]
-		$page: Int
-		$perPage: Int
-		$info: Boolean!
-	) {
-		player(id: $playerId) {
-			id
-			sets(filters: { tournamentIds: $tournamentsIds }, page: $page, perPage: $perPage) {
-				pageInfo @include(if: $info) {
-					total
-					totalPages
-				}
-
-				nodes @skip(if: $info) {
-					id
-					games {
-						id
-						selections {
-							id
-							entrant {
-								id
-							}
-							character {
-								id
-								name
-							}
-						}
-						stage {
-							id
-							name
-						}
-					}
-				}
-			}
-		}
-	}
-`);
+`;
+export const SearchPlayerByGamerTag = graphql(SearchPlayerByGamerTagQuery);
